642bca5641e2caacc7b52b91cd0cc93a46572b9a
Mensur Bektic <mensur.bektic@tui.de>
2019-10-08 22:28:38 +0200

    Added factory pattern.

---
+++ factory/Example.ts
@@ -0,0 +1,40 @@
+import Creator from "./Creator";
+import Product from "./Product";
+
+class ConcreteProduct1 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct1}';
+    }
+}
+
+class ConcreteProduct2 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct2}';
+    }
+}
+
+class ConcreteCreator1 extends Creator {
+    factoryMethod(): Product {
+        return new ConcreteProduct1();
+    }
+}
+
+class ConcreteCreator2 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct2();
+    }
+}
+
+function clientCode(creator: Creator) {
+    // ...
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+    // ...
+}
+
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2())
